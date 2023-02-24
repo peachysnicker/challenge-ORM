3062d8205d523bed9e62f7eecdfb6647bc7fa602
Mariah <110792371+peachysnicker@users.noreply.github.com>
2023-02-23 17:40:43 -0800

    finished tag routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -16,6 +16,7 @@ router.get('/', (req, res) => {
   }
 });
 
+
  // find a single tag by its `id`
   // be sure to include its associated Product data
   router.get('/:id', async (req, res) => {
@@ -44,9 +45,25 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+
+// update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!userData[0]) {
+      res.status(404).json({ message: 'No user with this id to update name by its id value!' });
+      return;
+    }
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
 
 // delete on tag by its `id` value
 router.delete('/:id', async (req, res) => {
