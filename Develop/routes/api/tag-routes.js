18bdcdf29dcb6f07a3006d6f6ba2eb4934ac08b8
Mariah <110792371+peachysnicker@users.noreply.github.com>
2023-02-25 19:21:26 -0800

    adding tag routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -1,1 +1,56 @@
+const router = require('express').Router();
+const { Product, Category, Tag, ProductTag } = require('../../models');
 
+// The `/api/tags` endpoint
+
+//GET all tags including Product data 
+router.get('/', async (req, res) => {
+    try {
+      const tagData = await Tag.findAll({
+        include: [{ model: Product, through: ProductTag }],
+      });
+      res.status(200).json(tagData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+
+//GET a single tag by id including product info
+router.get('/:id', async (req, res) => {
+    try {
+      const tagData = await Product.findByPk(req.params.id, {
+        include: [{model: Product, through: ProductTag
+       }],
+      });
+      if (!tagData) {
+        res.status(404).json({ message: 'No tag found with that id!' });
+        return;
+      }
+      res.status(200).json(tagData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+
+//CREATE a new product
+router.post('/', async (req, res) => {
+    try {
+      const newTag = await Tag.create(req.body);
+      res.status(200).json(newTag);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
