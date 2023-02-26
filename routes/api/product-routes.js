80cf6f9113b2658f722328bcde5dde95f5a9ce89
Mariah <110792371+peachysnicker@users.noreply.github.com>
2023-02-25 20:02:28 -0800

    fixed route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -11,7 +11,7 @@ router.get('/', async (req, res) => {
                 { model: Tag, through: ProductTag}
               ],
         })
-        res.status(200).json(locationData);
+        res.status(200).json(productData);
          } catch (err) {
      res.status(500).json(err);
     }
